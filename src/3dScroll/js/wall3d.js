ab8130a3951a713415fac80c28d41ee0f35ffa47
kdeveloper87 <k.developer87@gmail.com>
2019-12-29 23:10:57 +0900

    [add]window resize 시 값조정

--- src/3dScroll/js/wall3d.js
+++ src/3dScroll/js/wall3d.js
@@ -1,12 +1,20 @@
 (function () {
   const DEFAULT_Z = -490;
   const houseElem = document.querySelector( '.house' );
-  const maxScrollValue = document.body.offsetHeight - window.innerHeight;//스크롤할수있는 높이
+  let maxScrollValue = getMaxScrollValue();
 
   window.addEventListener( 'scroll', () => {
-    // console.log( pageYOffset / maxScrollValue);//스크롤비율
+    // console.log( pageYOffset ,  maxScrollValue);//스크롤비율
     const zMove = pageYOffset / maxScrollValue * 970 + DEFAULT_Z;
-    console.log( zMove );
     houseElem.style.transform = `translateZ(${ zMove }vw)`;
   } );
+
+  window.addEventListener( 'resize', () => {
+    maxScrollValue = getMaxScrollValue();
+  } );
+
+  function getMaxScrollValue() {
+    return document.body.offsetHeight - window.innerHeight;//스크롤할수있는 높이
+  }
+
 })();
