ffb3cb88b58e0421bc2d82992c66b2e15772ba50
kdeveloper87 <k.developer87@gmail.com>
2019-12-30 01:06:28 +0900

    [add]mouse move

--- src/3dScroll/js/wall3d.js
+++ src/3dScroll/js/wall3d.js
@@ -1,7 +1,9 @@
 (function () {
   const DEFAULT_Z_VALUE = -490;
+  const stageElem = document.querySelector( '.stage' );
   const houseElem = document.querySelector( '.house' );
   const progressBar = document.querySelector( '.progress-bar' );
+  const mousePos = { x: 0, y: 0 };
   let maxScrollValue = getMaxScrollValue();
 
   window.addEventListener( 'scroll', () => {
@@ -10,15 +12,27 @@
 
     houseElem.style.transform = `translateZ(${ zValue }vw)`;
     progressBar.style.width = `${ scrollPer * 100 }%`
-
   } );
 
   window.addEventListener( 'resize', () => {
     maxScrollValue = getMaxScrollValue();
   } );
 
+  window.addEventListener( 'mousemove', (e) => {
+    // console.log(e.clientX,e.clientY); 마우스 x,y
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform = `rotateX(${ mousePos.y * 5 }deg) rotateY(${ mousePos.x * 5 }deg)`;
+
+  } );
+
+
+  /**
+   * 실제 스크롤 높이
+   * @returns {number}
+   */
   function getMaxScrollValue() {
-    return document.body.offsetHeight - window.innerHeight;//스크롤할수있는 높이
+    return document.body.offsetHeight - window.innerHeight;
   }
 
 })();
